cbab68e296b851c443661a314609d506dcbb53fb
Edolove <edouardoantenorb.66@gmail.com>
2026-08-13 01:00:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const C="wick-v5-smart";
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="wick-v51";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
